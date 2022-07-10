7fc8fdc78efd95dd3fb78f483eec2994ae4bea1b
rei <rei0909rei@gmail.com>
2022-07-10 19:24:06 +0900

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<div>
+  <p>Todo List</p>
+  <form action="/" method="post">
+    <input type="text" name="todolist">
+    <input type="submit" value="追加">
+  </form>
+  <table>
+    <tr>
+      <th>作成日</th>
+      <th>タスク名</th>
+      <th>更新</th>
+      <th>削除</th>
+    </tr>
+  </table>
+</div>
